9867471d84e9604206599a47896ed9f86e1e0600
Bednar <gbed24@gmail.com>
2024-08-07 17:38:46 +0200

    feat(browser): Added first elements of browser control for TME site

---
+++ src/TME_website.py
@@ -0,0 +1,116 @@
+from selenium.common import StaleElementReferenceException, TimeoutException, NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from time import sleep
+
+
+class TME:
+    @staticmethod
+    def cookie_agree() -> str: return "[id='CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll']"
+
+    @staticmethod
+    def search_bar() -> str: return "[name='queryPhrase']"
+
+    @staticmethod
+    def search_submit_button() -> str: return "[class*='search-button']"
+
+    @staticmethod
+    def login_button() -> str: return "[aria-label='Zaloguj się']"
+
+    @staticmethod
+    def login_modal() -> tuple: return "[class*='login-modal__container']", 3
+
+    @staticmethod
+    def logout_wrapper() -> str: return "[class*='logout-wrapper']"
+
+    @staticmethod
+    def wait_for_user_to_login(driver: WebDriver):
+        while True:
+            driver.get("https://www.tme.eu/pl/")
+            Browser.wait_and_click(driver, TME.cookie_agree(), ignore_exception=True)
+            Browser.wait_and_click(driver, TME.login_button())
+            Browser.wait_for(driver, TME.login_modal())
+            print("\n> Please login to TME. Press enter when done.")
+            _ = input()
+            if Browser.wait_for(driver, TME.logout_wrapper()) is False and Browser.wait_for(driver, TME.login_button()) is True:
+                print("Not logged in. Try again!")
+            else:
+                print("Login successfull")
+                break
+
+
+class Browser:
+    @staticmethod
+    def wait_for_page():
+        sleep(1)
+
+    @staticmethod
+    def find(driver: WebDriver, locator) -> WebElement:
+        if isinstance(locator, str):
+            found = driver.find_element(By.CSS_SELECTOR, locator)
+            return found
+        else:
+            actual_locator = locator[0]
+            which_one = locator[1]
+            found = driver.find_element(By.CSS_SELECTOR, actual_locator)
+            if isinstance(found, WebElement):
+                return found
+            elif isinstance(found, list):
+                return found[locator[which_one]]
+
+    @staticmethod
+    def find_ignore_absence(driver: WebDriver, locator):
+        if isinstance(locator, str):
+            try:
+                found = driver.find_element(By.CSS_SELECTOR, locator)
+                return found
+            except NoSuchElementException:
+                return None
+        else:
+            actual_locator = locator[0]
+            which_one = locator[1]
+            try:
+                found = driver.find_element(By.CSS_SELECTOR, actual_locator)
+                if isinstance(found, WebElement):
+                    return found
+                elif isinstance(found, list):
+                    return found[locator[which_one]]
+            except NoSuchElementException:
+                return None
+
+    @staticmethod
+    def wait_for(driver: WebDriver, locator) -> bool:
+        if isinstance(locator, tuple):
+            actual_locator = locator[0]
+            which_one = locator[1]
+            try:
+                WebDriverWait(driver, 5).until(ec.presence_of_element_located((By.CSS_SELECTOR, actual_locator)))
+                return True
+            except TimeoutException:
+                return False
+        else:
+            try:
+                WebDriverWait(driver, 5).until(ec.presence_of_element_located((By.CSS_SELECTOR, locator)))
+                return True
+            except TimeoutException:
+                return False
+
+    @staticmethod
+    def wait_and_click(driver: WebDriver, locator, ignore_exception: bool = False) -> None:
+        Browser.wait_for_page()
+        Browser.wait_for(driver, locator)
+        if ignore_exception:
+            element = Browser.find_ignore_absence(driver, locator)
+            if element:
+                element.click()
+        else:
+            Browser.find(driver, locator).click()
+
+    @staticmethod
+    def wait_and_input(driver: WebDriver, locator, input_string: str) -> None:
+        Browser.wait_for_page()
+        Browser.wait_for(driver, locator)
+        Browser.find(driver, locator).send_keys(input_string)
--- src/run.py
+++ src/run.py
@@ -1,58 +1,96 @@
 import os
 import csv
+from selenium import webdriver
+
 from prototypes import Component, ComponentType, ColumnName
+from TME_website import TME
 
 # global variables
+skip_to_browser = True
+
 components = []
 file_path = None  # C:\Users\Grzesiek\Desktop\BOM_Buck-25W-V3-24VCharger_2024-08-07.csv
 is_path_incorrect = True
+user_input = [None, None]
 
 
-print("\n\nWelcome to AutoBOM\n")
+def page_has_loaded(web_driver: webdriver):
+    page_state = web_driver.execute_script('return document.readyState;')
+    return page_state == 'complete'
 
-while is_path_incorrect:
-    print("> Please input full path to EasyEDA BOM CSV file:")
-    file_path = input().replace(os.sep, '/')
-    if os.path.exists(file_path):
-        is_path_incorrect = False
-        print("> Opening CSV file")
-    else:
-        print("\n> Invalid path. Try again.")
 
-with open(file_path, newline='', encoding='utf-16') as csvfile:
-    reader = csv.reader(csvfile, delimiter='\t', quotechar='|')
-    for index, row in enumerate(reader):
-        if index == 0:
-            column_names_temp = []
-            for i, element in enumerate(row):
-                column_names_temp.append(element)
-            column_names = dict.fromkeys(column_names_temp)
+print("\n\nWelcome to AutoBOM")
+
+if not skip_to_browser:
+    while is_path_incorrect:
+        print("\n> Please input full path to EasyEDA BOM CSV file:")
+        file_path = input().replace(os.sep, '/')
+        if os.path.exists(file_path):
+            is_path_incorrect = False
+            print("> Opening CSV file")
         else:
-            new_component_name = None
-            new_component_designators = None
-            new_component_footprint = None
-            new_component_quantity = None
-            new_component_typeof = None
+            print("> Invalid path. Try again.")
 
-            for i, element in enumerate(row):
-                if list(ColumnName)[i].value == ColumnName.NAME.value:
-                    new_component_name = element
-                elif list(ColumnName)[i].value == ColumnName.DESIGNATOR.value:
-                    new_component_designators = element.strip('"').split(",")
-                elif list(ColumnName)[i].value == ColumnName.FOOTPRINT.value:
-                    new_component_footprint = element
-                elif list(ColumnName)[i].value == ColumnName.QUANTITY.value:
-                    new_component_quantity = int(element.strip('"'))
+    with open(file_path, newline='', encoding='utf-16') as csvfile:
+        reader = csv.reader(csvfile, delimiter='\t', quotechar='|')
+        for index, row in enumerate(reader):
+            if index == 0:
+                column_names_temp = []
+                for i, element in enumerate(row):
+                    column_names_temp.append(element)
+                column_names = dict.fromkeys(column_names_temp)
+            else:
+                new_component_name = None
+                new_component_designators = None
+                new_component_footprint = None
+                new_component_quantity = None
+                new_component_typeof = None
 
-            # recognize type
-            designator_letter = new_component_designators[0][0]
-            for i, typeof in enumerate(list(ComponentType)):
-                if designator_letter == typeof.value:
-                    new_component_typeof = typeof
-            components.append(Component(name=new_component_name, designators=new_component_designators,
-                                        footprint=new_component_footprint, quantity=new_component_quantity,
-                                        typeof=new_component_typeof))
+                for i, element in enumerate(row):
+                    if list(ColumnName)[i].value == ColumnName.NAME.value:
+                        new_component_name = element
+                    elif list(ColumnName)[i].value == ColumnName.DESIGNATOR.value:
+                        new_component_designators = element.strip('"').split(",")
+                    elif list(ColumnName)[i].value == ColumnName.FOOTPRINT.value:
+                        new_component_footprint = element
+                    elif list(ColumnName)[i].value == ColumnName.QUANTITY.value:
+                        new_component_quantity = int(element.strip('"'))
 
-print("\n> Parsed components:")
-for index, component in enumerate(components):
-    component.printout(index + 1)
+                # recognize type
+                designator_letter = new_component_designators[0][0]
+                for i, typeof in enumerate(list(ComponentType)):
+                    if designator_letter == typeof.value:
+                        new_component_typeof = typeof
+                components.append(Component(name=new_component_name, designators=new_component_designators,
+                                            footprint=new_component_footprint, quantity=new_component_quantity,
+                                            typeof=new_component_typeof))
+
+    print("\n> Parsed components:")
+    for index, component in enumerate(components):
+        component.printout(index + 1)
+
+    print("\n> Please verify the above list. Is it correct? [y/n]:")
+    user_control = input()
+else:
+    user_control = "Y"
+
+if user_control in ["y", "Y"]:
+    print("\n> Proceeding with web browser activity")
+
+    # browser setup
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--disable-search-engine-choice-screen")
+    chrome_options.add_argument("--start-maximized")
+    browser = webdriver.Chrome(options=chrome_options)
+
+    # navigate to the login page
+    TME.wait_for_user_to_login(browser)
+
+    # TODO: - if koszyk is not empty, go and empty it
+    #  - if koszyk is empty, go into a loop for all components in a list
+    #  - loop: for each component - search for name, with available now filter selected and correct footprints
+    #  - if found, add it to cart, if found multiple select cheapest, if not found try to find similar
+    #  (or save what wasn't found for later)
+
+else:
+    print("> List not correct, aborting...")
